94afecdca6a99c135055b69b1b3e7dbeefa28cc5
trang204 <ngtrang2204@gmail.com>
2025-08-15 22:31:24 +0700

    sua loi

--- Lab07/Lab7.1/app/src/main/java/com/example/intern/MainActivity.java
+++ Lab07/Lab7.1/app/src/main/java/com/example/intern/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 
 import androidx.activity.EdgeToEdge;
